4c9a618b6b815b203979ea4cb996b8553242815b
Chris Colvard <chris.colvard@gmail.com>
2020-12-15 17:37:54 -0500

    Appease rubocop

--- lib/blacklight_oai_provider/solr_document_wrapper.rb
+++ lib/blacklight_oai_provider/solr_document_wrapper.rb
@@ -21,13 +21,13 @@ module BlacklightOaiProvider
     def earliest
       builder = @controller.search_builder.merge(fl: solr_timestamp, sort: "#{solr_timestamp} asc", rows: 1)
       response = @controller.repository.search(builder)
-      response.documents.first&.timestamp || Time.now
+      response.documents.first&.timestamp || Time.now.utc
     end
 
     def latest
       builder = @controller.search_builder.merge(fl: solr_timestamp, sort: "#{solr_timestamp} desc", rows: 1)
       response = @controller.repository.search(builder)
-      response.documents.first&.timestamp || Time.now
+      response.documents.first&.timestamp || Time.now.utc
     end
 
     def find(selector, options = {})
